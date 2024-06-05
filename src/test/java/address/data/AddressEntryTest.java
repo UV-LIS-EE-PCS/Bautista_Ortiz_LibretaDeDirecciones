20547a5a284c0fa0f8841d74afba0f18242eba83
Ortiz05Ale <bautistaortizalejandra@gmail.com>
2024-06-04 22:26:45 -0600

    [Test]:add all test methods of the required classes

--- src/test/java/address/data/AddressEntryTest.java
+++ src/test/java/address/data/AddressEntryTest.java
@@ -6,17 +6,60 @@ import address.data.AddressEntry;
 
 public class AddressEntryTest {
 
+    /**
+     * Test to validate the constructor and getters of the AddressEntry class
+     */
     @Test
     void testConstructorAndGetters() {
-        AddressEntry entry = new AddressEntry("Alejandra", "Ortiz", "Tulipanes", "");
-        /*AddressEntry entry = new AddressEntry("John", "Doe", "123 Main St", "Anytown", "NY", 12345, "john@example.com", "1234567890");
-        assertEquals("John", entry.getName());
-        assertEquals("Doe", entry.getLastName());
-        assertEquals("123 Main St", entry.getStreet());
-        assertEquals("Anytown", entry.getCity());
-        assertEquals("NY", entry.getState());
-        assertEquals(12345, entry.getZip());
-        assertEquals("john@example.com", entry.getEmail());
-        assertEquals("1234567890", entry.getPhoneNumber());*/
+        AddressEntry entry = new AddressEntry("Alejandra", "Ortiz", "Tulipanes", "Minatitlán",
+                "Veracruz", 96870, "bautista@gmail.com", "9221859943");
+        assertEquals("Alejandra", entry.getName());
+        assertEquals("Ortiz", entry.getLastName());
+        assertEquals("Tulipanes", entry.getStreet());
+        assertEquals("Minatitlán", entry.getCity());
+        assertEquals("Veracruz", entry.getState());
+        assertEquals(96870, entry.getZip());
+        assertEquals("bautista@gmail.com", entry.getEmail());
+        assertEquals("9221859943", entry.getPhoneNumber());
     }
+
+    /**
+     * Test to validate the setters of the AddressEntry class
+     */
+    @Test
+    void testSettersAndGetters() {
+        AddressEntry entry = new AddressEntry();
+        entry.setName("Alejandra");
+        entry.setLastName("Ortiz");
+        entry.setStreet("Tulipanes");
+        entry.setCity("Minatitlán");
+        entry.setState("Veracruz");
+        entry.setZip(96870);
+        entry.setEmail("bautista@gmail.com");
+        entry.setPhoneNumber("9221859943");
+        assertEquals("Alejandra", entry.getName());
+        assertEquals("Ortiz", entry.getLastName());
+        assertEquals("Tulipanes", entry.getStreet());
+        assertEquals("Minatitlán", entry.getCity());
+        assertEquals("Veracruz", entry.getState());
+        assertEquals(96870, entry.getZip());
+        assertEquals("bautista@gmail.com", entry.getEmail());
+        assertEquals("9221859943", entry.getPhoneNumber());
+    }
+
+
+    /**
+     * Test to validate the toString method of the AddressEntry class
+     */
+    @Test
+    void testToString(){
+        AddressEntry entry = new AddressEntry("Alejandra", "Ortiz", "Tulipanes", "Minatitlán",
+                "Veracruz", 96870, "bautista@gmail.com", "9221859943");
+        assertEquals("Name: Alejandra\nLast Name: Ortiz\nStreet: Tulipanes\nCity: Minatitlán\nState: Veracruz" +
+                "\nZip: 96870\nEmail: bautista@gmail.com\nPhoneNumber: 9221859943", entry.toString());
+
+
+    }
+
 }
+
